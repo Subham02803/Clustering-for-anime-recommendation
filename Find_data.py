d1142f17ea72d8799254d970607e82cacc4f06ad
Subham02803 <subhambiswas688@gmail.com>
2019-10-21 03:35:45 +0530

    pikle file added

--- Find_data.py
+++ Find_data.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import pickle
 
 anime = pd.read_csv('anime.csv')
 anime_name = anime['name']
 anime_genre = anime['genre']
+
+pkl_file = open('myfile.pkl', 'rb')
+df = pickle.load(pkl_file)
+pkl_file.close()
 
 def get_genre(name):
     for i,x in enumerate(anime_name.values):
